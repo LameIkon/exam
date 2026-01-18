ef1fbc1103a9711ed9dfb2705cb317e905d80f15
LameIkon <troelsjelsbak@hotmail.com>
2026-01-18 18:07:09 +0100

    I'm done

---
+++ api-fetcher.js
@@ -0,0 +1,14 @@
+﻿fetch("https://api.eksempel.dk/data", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer minapikey123456"
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log("API-svar:", data);
+    })
+    .catch(error => {
+        console.error("Fejl ved API-kald:", error);
+    });
---
+++ encrypter.js
@@ -0,0 +1,22 @@
+﻿import bcrypt from "bcrypt";
+
+// Simuleret POST-request body
+const postedPassword = req.body.password;
+
+// Hashet password hentet fra databasen (eksempel)
+const hashedPasswordFromDB = user.password; // fx "$2b$10$abc123..."
+
+async function checkPassword(postedPassword, hashedPasswordFromDB) {
+    const isMatch = await bcrypt.compare(postedPassword, hashedPasswordFromDB);
+
+    if (isMatch) {
+        console.log("Password er korrekt");
+        return true;
+    } else {
+        console.log("Password er forkert");
+        return false;
+    }
+}
+
+// Brug funktionen
+checkPassword(postedPassword, hashedPasswordFromDB);
